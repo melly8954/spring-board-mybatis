e1836509a481baf6724e202560de240a9742e16b
haily <49448330+melly8954@users.noreply.github.com>
2025-11-22 21:35:56 +0900

    feat: init

---
+++ src/test/java/com/equip/spring_board_mybatis/SpringBoardMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equip.spring_board_mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
