79e8b225e4b4817730bf0a819cc8a8ee30609903
haily <49448330+melly8954@users.noreply.github.com>
2025-11-23 20:10:46 +0900

    feat: 외부 리소스 config 등록

---
+++ src/main/java/com/spBoard/spring_board_mybatis/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.spBoard.spring_board_mybatis.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final FileConfig fileConfig;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("file:" + fileConfig.getStoragePath() + "/");
+    }
+}
